b3df0a77b364f6b7b28af295a2dd8dff26f594a4
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-02 17:51:01 +0900

    test: queryParam 을 통한 User 인스턴스 생성 테스트 추가

---
+++ src/test/java/model/UserTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import mock.MockRequest;
+import org.junit.Test;
+import util.HttpRequestUtils;
+import util.PathUtils;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void queryParam으로부터_User인스턴스를_만들_수_있다() {
+        Map<String, String> queryParam =
+                HttpRequestUtils.parseQueryString(
+                        PathUtils.queryString(MockRequest.requestedStr()));
+
+        User user = new User(
+                queryParam.get("userId"),
+                queryParam.get("password"),
+                queryParam.get("name"),
+                queryParam.get("email"));
+
+        assertNotNull(user);
+    }
+
+}
