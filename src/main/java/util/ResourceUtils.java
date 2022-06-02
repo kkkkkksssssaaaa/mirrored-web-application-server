a9c28deba9251abd2a286bda02fcfd698d58fe64
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-02 23:48:03 +0900

    refactor: 요청한 리소스에 따라 해당 리소스 반환

--- src/main/java/util/ResourceUtils.java
+++ src/main/java/util/ResourceUtils.java
@@ -14,43 +14,16 @@ public class ResourceUtils {
     private static final Logger log = LoggerFactory.getLogger(ResourceUtils.class);
 
     public static byte[] staticResourceBytes(String line) {
-        if (ResourceValidator.isContainQueryParameter(line)) {
-            // TODO 정적 리소스(.html) 유지하여 응답해야함
+        if (!ResourceValidator.isPresent(staticResourcePath(line))) {
             return new byte[0];
         }
 
-        if (!ResourceValidator.isPresent(staticResource(line))) {
-            return new byte[0];
-        }
-
-        try {
-            return Files
-                    .readAllBytes(resource(
-                            staticResource(line)).toPath());
-        } catch (IOException e) {
-            log.error("{} is invalid resource.", line);
-
-            return new byte[0];
-        }
+        return bytes(staticResourcePath(line));
     }
 
     public static Map<String, String> queryParams(String line) {
-        return HttpRequestUtils.parseQueryString(
-                queryString(line));
-    }
-
-    private static File resource(String path) {
-        return new File(ResourceValidator.DEFAULT_PATH + path);
-    }
-
-    public static String staticResource(String requestedStr) {
-        Matcher m = ResourceValidator.matcher(requestedStr);
-
-        if (!m.find()) {
-            throw new IllegalArgumentException();
-        }
-
-        return m.group(2);
+        return HttpRequestUtils
+                .parseQueryString(queryString(line));
     }
 
     public static String queryString(String requestedStr) {
@@ -62,4 +35,33 @@ public class ResourceUtils {
 
         return m.group(3).replace("?", "");
     }
+
+    private static File staticResource(String path) {
+        return new File(ResourceValidator.DEFAULT_PATH + path);
+    }
+
+    public static String staticResourcePath(String line) {
+        Matcher m = ResourceValidator.matcher(line);
+
+        if (!m.find()) {
+            throw new IllegalArgumentException();
+        }
+
+        return m.group(2);
+    }
+
+    private static byte[] bytes(String path) {
+        try {
+            File staticResource = staticResource(
+                    ResourceValidator.findStaticResource(path));
+
+            return Files
+                    .readAllBytes(staticResource.toPath());
+        } catch (IOException e) {
+            log.error("{} is invalid resource.", path);
+
+            return new byte[0];
+        }
+    }
+
 }
