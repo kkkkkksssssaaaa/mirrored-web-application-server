baef78379e6cccb3cfe72e1921bb2cad59c6d8a8
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-03 01:25:23 +0900

    feat: 302 응답 헤더에 메인 페이지 로케이션 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -72,6 +72,7 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("Location: " + ResourceValidator.MAIN);
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
