c58e968ad90548528f314b035f3c509fd2de7e3f
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-02 16:31:47 +0900

    feat: PathUtils 추가

---
+++ src/main/java/util/PathUtils.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PathUtils {
+
+    private static final Pattern REQUEST_PATTERN =
+            Pattern.compile("(GET|POST|PUT|DELETE)\\s+([^?\\s]+)((?:[?&][^&\\s]+)*)\\s+(HTTP/.*)");
+
+    public static boolean isRequestPatternMatched(String requestedStr) {
+        return REQUEST_PATTERN.matcher(requestedStr).find();
+    }
+
+    public static String getRequestedResourcePath(String requestedStr) {
+        Matcher m = REQUEST_PATTERN.matcher(requestedStr);
+
+        if (!m.find()) {
+            throw new IllegalArgumentException();
+        }
+
+        return m.group(2);
+    }
+
+}
