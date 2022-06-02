6f7875d307727e0d0ec5b4a1ee05d775b3d7118e
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-03 00:42:20 +0900

    feat: Request 모델 생성

---
+++ src/main/java/model/Request.java
@@ -0,0 +1,57 @@
+package model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.StringUtils;
+import webserver.RequestHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Request {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+    private final List<String> lines = new ArrayList<>();
+
+    private Request(InputStream in) {
+        fillLines(in);
+    }
+
+    public static Request fromInputStream(InputStream in) {
+        return new Request(in);
+    }
+
+    public String resource() {
+        return this.lines.get(0);
+    }
+
+    private void fillLines(InputStream in) {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(in, DEFAULT_CHARSET));
+
+            String line = null;
+
+            System.out.println("\n=======================================");
+
+            while (StringUtils.isPresent(line = reader.readLine())) {
+                this.lines.add(line);
+                System.out.println(line);
+            }
+
+            System.out.println("\n=======================================");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+
+            throw new RuntimeException();
+        }
+    }
+
+}
