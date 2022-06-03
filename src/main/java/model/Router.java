b4e908b2e6fc88681f8d4210982cacdf0802486f
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-03 17:20:52 +0900

    feat: 로그인/회원가입 라우터 추가

--- src/main/java/model/Router.java
+++ src/main/java/model/Router.java
@@ -6,16 +6,27 @@ import java.util.Map;
 public class Router {
 
     public static final String MAIN = "/index.html";
+    public static final String USER_CREATE = "/user/create";
     public static final String USER_FORM = "/user/form.html";
     public static final String USER_LOGIN = "/user/login.html";
+    public static final String USER_LOGIN_FAILED = "/user/login_failed.html";
 
     private static final Map<String, String> pages = new HashMap<>();
 
     static {
-        pages.put("/index.html", MAIN);
-        pages.put("/user/create", USER_FORM);
-        pages.put("/user/form.html", USER_FORM);
-        pages.put("/user/login.html", USER_LOGIN);
+        pages.put(MAIN, MAIN);
+        pages.put(USER_CREATE, USER_FORM);
+        pages.put(USER_FORM, USER_FORM);
+        pages.put(USER_LOGIN, USER_LOGIN);
+        pages.put(USER_LOGIN_FAILED, USER_LOGIN_FAILED);
+    }
+
+    public static String find(String key) {
+        if (!pages.containsKey(key)) {
+            throw new IllegalArgumentException();
+        }
+
+        return pages.get(key);
     }
 
 }
