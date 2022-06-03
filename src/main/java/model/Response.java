f9b4711de9bf6716f805e48c8cc686f35b979732
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-03 16:09:00 +0900

    feat: Response 모델 생성

---
+++ src/main/java/model/Response.java
@@ -0,0 +1,80 @@
+package model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Response {
+
+    private static final Logger log = LoggerFactory.getLogger(Response.class);
+    private final int code;
+    private final byte[] body;
+    private final OutputStream out;
+    private final DataOutputStream dos;
+
+    private static final Map<Integer, String> acceptCodes = new HashMap<>();
+
+    static {
+        acceptCodes.put(200, "OK");
+        acceptCodes.put(302, "Found");
+    }
+
+    private Response(int code,
+                     byte[] body,
+                     OutputStream out) {
+        if (acceptCodes.containsKey(code)) {
+            throw new IllegalArgumentException("not found status");
+        }
+
+        this.code = code;
+        this.body = body;
+        this.out = out;
+
+        this.dos = new DataOutputStream(out);
+    }
+
+    public static Response create(int code,
+                                  byte[] body,
+                                  OutputStream out) {
+        return new Response(code, body, out);
+    }
+
+    private void writeHeader() {
+        try {
+            dos.writeBytes(head());
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + this.body.length + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void writeBody() {
+        try {
+            dos.write(this.body, 0, this.body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private String head() {
+        return String.format("HTTP/1.1 %d %s \r\n", status().getKey(), status().getValue());
+    }
+
+    private Map.Entry<Integer, String> status() {
+        return acceptCodes
+                .entrySet()
+                .stream()
+                .filter(x -> x.getKey().equals(this.code))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("not found status"));
+    }
+
+}
