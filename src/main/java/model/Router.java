6b925a5a934a228e2056325dd5d1e3b269269642
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-03 19:28:33 +0900

    refactor: 라우터 상수명 중 페이지 상수 구분하여 rename

--- src/main/java/model/Router.java
+++ src/main/java/model/Router.java
@@ -1,26 +1,24 @@
 package model;
 
-import util.ResourceUtils;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Router {
 
-    public static final String MAIN = "/index.html";
+    public static final String PAGE_MAIN = "/index.html";
+    public static final String PAGE_USER_FORM = "/user/form.html";
+    public static final String PAGE_USER_LOGIN = "/user/login.html";
+    public static final String PAGE_USER_LOGIN_FAILED = "/user/login_failed.html";
     public static final String USER_CREATE = "/user/create";
-    public static final String USER_FORM = "/user/form.html";
-    public static final String USER_LOGIN = "/user/login.html";
-    public static final String USER_LOGIN_FAILED = "/user/login_failed.html";
 
     private static final Map<String, String> pages = new HashMap<>();
 
     static {
-        pages.put(MAIN, MAIN);
-        pages.put(USER_CREATE, USER_FORM);
-        pages.put(USER_FORM, USER_FORM);
-        pages.put(USER_LOGIN, USER_LOGIN);
-        pages.put(USER_LOGIN_FAILED, USER_LOGIN_FAILED);
+        pages.put(PAGE_MAIN, PAGE_MAIN);
+        pages.put(USER_CREATE, PAGE_USER_FORM);
+        pages.put(PAGE_USER_FORM, PAGE_USER_FORM);
+        pages.put(PAGE_USER_LOGIN, PAGE_USER_LOGIN);
+        pages.put(PAGE_USER_LOGIN_FAILED, PAGE_USER_LOGIN_FAILED);
     }
 
     public static String find(String key) {
