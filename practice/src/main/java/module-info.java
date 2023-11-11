403d988b4149f0e19cc91747d630b0e9c65da4cd
kkkkkksssssaaaa <kofc312@naver.com>
2023-11-11 22:04:10 +0900

    feat: migrate maven to gradle

---
+++ practice/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module dev.kkkkkksssssaaaa.javawas.practice {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens dev.kkkkkksssssaaaa.javawas.practice to javafx.fxml;
+    exports dev.kkkkkksssssaaaa.javawas.practice;
+}
